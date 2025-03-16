924f5992c1def986ec7757aea74fad9848a9141a
gmarchetx <gregoire.marchetaux@reactiondynamics.space>
2025-03-16 02:14:10 -0400

    First draft without fprime_app

---
+++ fprime_demo/scripts/run.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+#Build fprime deployment
+. ./fprime-venv/bin/activate
+pip install setuptools --break-system-packages #a bug in the requirements.txt of fprime, see https://github.com/nasa/fprime/issues/3101
+cd fprime/Ref
+printf "yes\nyes\nyes" | fprime-util purge
+fprime-util generate
+fprime-util build -j8
+
+#Generate YAMCS mdb
+python3 /src/fprime2yamcs/main.py --fprime-artifacts "$FP_ARTIFACTS" --yamcs-mdb "$YAMCS_MDB"
+
+#Run deployment
+chmod +x build-artifacts/Linux/Ref/bin/Ref
+#./build-artifacts/Linux/Ref/bin/Ref -a "127.0.0.1" -p $PORT_TC #TODO: update name/path if needed, and add PORT_TM (potentially PORT_EV)
