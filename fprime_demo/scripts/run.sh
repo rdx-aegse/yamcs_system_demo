d29fcc2f4eeee12144468e39e1d2867b99e1df95
gmarchetx <gregoire.marchetaux@reactiondynamics.space>
2025-03-26 02:00:08 -0400

    Rewinding, git is bloody annoying again

--- fprime_demo/scripts/run.sh
+++ fprime_demo/scripts/run.sh
@@ -1,16 +1,15 @@
 #!/bin/sh
 
 #Build fprime deployment
-. ./fprime-venv/bin/activate
-pip install setuptools --break-system-packages #a bug in the requirements.txt of fprime, see https://github.com/nasa/fprime/issues/3101
-cd fprime/Ref
+. /fprime_app/fprime-venv/bin/activate
+#pip install setuptools --break-system-packages #a bug in the requirements.txt of fprime, see https://github.com/nasa/fprime/issues/3101
 printf "yes\nyes\nyes" | fprime-util purge
 fprime-util generate
 fprime-util build -j8
 
 #Generate YAMCS mdb
-python3 /src/fprime2yamcs/main.py --fprime-artifacts "$FP_ARTIFACTS" --yamcs-mdb "$YAMCS_MDB"
+python3 /fprime2yamcs/main.py --fprime-artifacts "/fprime_app/demo/build-artifacts/demo/dict" --fprime-topology "/fprime_app/demo/Top" --yamcs-mdb "$YAMCS_MDB"
 
 #Run deployment
-chmod +x build-artifacts/Linux/Ref/bin/Ref
+#chmod +x build-artifacts/Linux/Ref/bin/Ref
 #./build-artifacts/Linux/Ref/bin/Ref -a "127.0.0.1" -p $PORT_TC #TODO: update name/path if needed, and add PORT_TM (potentially PORT_EV)
