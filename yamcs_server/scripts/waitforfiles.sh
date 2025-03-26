909e5622709287bba137ce5f159a2004132c0b53
gmarchetx <gregoire.marchetaux@reactiondynamics.space>
2025-03-26 03:01:53 -0400

    Fprime compiles and generates mdbs

--- yamcs_server/scripts/waitforfiles.sh
+++ yamcs_server/scripts/waitforfiles.sh
@@ -6,7 +6,6 @@ if [ $# -eq 0 ]; then
 fi
 
 folder_path="$1"
-timeout=30  # seconds
 
 if [ ! -d "$folder_path" ]; then
     echo "Error: '$folder_path' is not a valid directory."
@@ -15,14 +14,8 @@ fi
 
 echo "Waiting for files in folder '$folder_path'..."
 
-t=0
 while [ -z "$(ls -A "$folder_path")" ]; do
     sleep 1
-    ((t++))
-    if [ $t -ge $timeout ]; then
-        echo "Timeout reached. Folder is still empty."
-        exit 1
-    fi
 done
 
 echo "Folder is no longer empty. Resuming execution"
